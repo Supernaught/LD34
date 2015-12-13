a89d7054c84da4bf1a03e45456de32a3076186eb
Davy Bolivar <davy.bolivar@gmail.com>
2015-12-13 16:01:21 +0800

    Add Spritesheet and Animations

--- source/Reg.hx
+++ source/Reg.hx
@@ -19,13 +19,23 @@ class Reg
 	// Assets
 	public inline static var TILESHEET:String = "assets/images/tiles.png";
 	public inline static var GIBS_SPRITESHEET:String = "assets/images/gibs.png";
-	public inline static var PLAYER_SPRITESHEET:String = "assets/images/tiles.png";
+	public inline static var SPRITESHEET:String = "assets/images/tileset.png";
 
 	public static function getPlayerAnim(Player:FlxSprite){
-		Player.loadGraphic(PLAYER_SPRITESHEET, true, 16,16);
+		Player.loadGraphic(SPRITESHEET, true, 16,16);
 		// Player.animation.add("idle", [0,1,2,3], 10);
-		Player.animation.add("run", [1], 18);
-		Player.animation.add("fall", [3], 13);
-		Player.animation.add("jump", [2], 13);
+		Player.animation.add("run", [70,71,72,73,74], 15);
+		Player.animation.add("fall", [80,81,82], 20);
+		Player.animation.add("jump", [75,76,77,78,79], 20);
+	}
+
+	public static function getPowerUpAnim(PowerUp:FlxSprite){
+		PowerUp.loadGraphic(SPRITESHEET, true, 16,16);
+		PowerUp.animation.add("bounce", [48,49,50,51,52,53,54,55,56], 30);
+	}
+
+	public static function getTrails(Trail:FlxSprite){
+		Trail.loadGraphic(SPRITESHEET, true, 16,16);
+		Trail.animation.add("jumpTrail", [83,84,85,86,87], 30);
 	}
 }
